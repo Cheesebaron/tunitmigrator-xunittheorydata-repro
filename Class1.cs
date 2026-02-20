a51bee453e7f4d7f5c2a17b9f3569de3e7ae5fea
Tomasz Cielecki <249719+Cheesebaron@users.noreply.github.com>
2026-02-20 12:10:53 +0100

    example

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace tunitmigrator_xunittheorydata_repro;
+
+public class ExampleTests
+{
+    [Theory]
+    [MemberData(nameof(MyData))]
+    public void MyTest(
+        string a, int b)
+    {
+        Assert.NotNull(a);
+        Assert.NotEqual(0, b);   
+    }
+
+    public static TheoryData<string, int> MyData
+    {
+        get
+        {
+            var data = new TheoryData<string, int>
+            {
+                {
+                    "a",
+                    1
+                },
+            };
+
+            return data;
+        }
+    }
+}
